a2520b2a6772af894bdb6f8d71f6df04a2486557
darkmuminek <bartosz.stodolny@gmail.com>
2022-07-22 19:26:21 +0200

    Kielce_final_1.0

---
+++ uruchom.ahk
@@ -0,0 +1,8 @@
+﻿SetTitleMatchMode, 2
+if WinExist("Centrum Edukacji Medialnej w Kielcach")
+	WinActivate
+Sleep, 10000
+ControlClick, x945 y253, Centrum Edukacji Medialnej w Kielcach
+Sleep, 5000
+SendInput, !1
+return
---
+++ uruchom.bat
@@ -0,0 +1,7 @@
+@Echo Off
+@Echo "Program uruchomi sie za 60 sekund, prosze nie restartowac komputera oraz nic nie naciskac"
+Timeout /t 60 /nobreak
+@Echo Off
+E:\Onedrive\Dokumenty\GitHub\Kielce_final\index.html
+Timeout /10 /nobreak
+E:\Onedrive\Dokumenty\GitHub\Kielce_final\uruchom.exe
